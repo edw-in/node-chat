bbcf01d7f2637d339303a5619f674a5bddafe908
Edwin Reji <edwinnreji@gmail.com>
2020-12-22 16:22:55 +0000

    Added few UI changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+app.use(express.static('views'));
+
 app.get('/', function(req, res) {
     res.render('index.ejs');
 });
@@ -10,7 +12,7 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+        io.emit('is_online', '🔵 <i>' + socket.username + ' joined the chat..</i>');
     });
 
     socket.on('disconnect', function(username) {
